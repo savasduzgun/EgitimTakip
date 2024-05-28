e36723fdbcbbf388ed6af47348fa2a759ee67982
savasduzgun <savasdzgn@gmail.com>
2024-05-28 22:47:23 +0300

    Proje katmanlara ayrıldı. Models katmanı oluşturuldu. Models katmanında base model ve diğer classlar oluşturuldu.

---
+++ EgitimTakip.Models/Company.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EgitimTakip.Models
+{
+    public class Company : BaseModel
+    {
+        public string Name { get; set; }
+        public int UserId { get; set; }
+        public virtual AppUser User { get; set; }
+        public virtual ICollection<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
