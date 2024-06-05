dc7e4c37cc07319e20608a82235486590ab4f74a
savasduzgun <savasdzgn@gmail.com>
2024-06-06 00:01:04 +0300

    traininSubject listelemek için GetAll action ı yazıldı

--- EgitimTakip.Web/Controllers/TrainingSubjectController.cs
+++ EgitimTakip.Web/Controllers/TrainingSubjectController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EgitimTakip.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EgitimTakip.Web.Controllers
 {
     public class TrainingSubjectController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public TrainingSubjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult GetAll()
+        {
+            var result = _context.TrainingSubjects.ToList();
+            return Json(new { data = result }); //datatable da kullanabilmek için
         }
     }
 }
