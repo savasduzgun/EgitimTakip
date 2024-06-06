1d0784bb4d6f2f9d0c7ea7cde9ca74a6b2348241
savasduzgun <savasdzgn@gmail.com>
2024-06-07 00:06:16 +0300

    TrainingController oluşturuldu.

---
+++ EgitimTakip.Web/Controllers/TrainingController.cs
@@ -0,0 +1,20 @@
+﻿using EgitimTakip.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EgitimTakip.Web.Controllers
+{
+    public class TrainingController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TrainingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
