b3bc7b5c4ad8c0bc69f9fc85f394f6c282edd550
savasduzgun <savasdzgn@gmail.com>
2024-06-06 23:45:44 +0300

    Şirketlerin çalışan listelerini getirecek GetAll metodu yazıldı.

--- EgitimTakip.Web/Controllers/EmployeeController.cs
+++ EgitimTakip.Web/Controllers/EmployeeController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EgitimTakip.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EgitimTakip.Web.Controllers
 {
     public class EmployeeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult GetAll(int companyId)
+        {
+            var result = _context.Employees.Where(e => e.CompanyId == companyId && !e.IsDeleted).ToList();
+            return Json(new { data = result });
         }
     }
 }
