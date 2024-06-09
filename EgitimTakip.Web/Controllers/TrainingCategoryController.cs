27b8f3b189d5904a8417bf508bba1d478e8f2e5b
savasduzgun <savasdzgn@gmail.com>
2024-06-09 23:44:08 +0300

    TrainingCategory controller oluşturuldu

---
+++ EgitimTakip.Web/Controllers/TrainingCategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EgitimTakip.Web.Controllers
+{
+    public class TrainingCategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
