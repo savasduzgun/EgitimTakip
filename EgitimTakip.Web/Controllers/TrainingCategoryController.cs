ac2987445dabe12945515b331be5eb8ba0bf84a9
savasduzgun <savasdzgn@gmail.com>
2024-06-09 23:45:31 +0300

    ApplicattionDbContext i dependency injection ile alındı

--- EgitimTakip.Web/Controllers/TrainingCategoryController.cs
+++ EgitimTakip.Web/Controllers/TrainingCategoryController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EgitimTakip.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EgitimTakip.Web.Controllers
 {
     public class TrainingCategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public TrainingCategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
