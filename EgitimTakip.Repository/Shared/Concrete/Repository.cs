32de06d877d47bda2610983ed52c10651fd86c75
savasduzgun <savasdzgn@gmail.com>
2024-08-01 15:21:16 +0300

    Concrete klasörüne Repository classı eklendi.

---
+++ EgitimTakip.Repository/Shared/Concrete/Repository.cs
@@ -0,0 +1,53 @@
+﻿using EgitimTakip.Repository.Shared.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EgitimTakip.Repository.Shared.Concrete
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public T Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T AddRange(List<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
